207771e82cd59eada816baf21627070cd927fdf5
DLK <dkratzk@gmail.com>
2018-06-01 18:26:36 -0500

    new options

--- GameComponents.py
+++ GameComponents.py
@@ -1,8 +1,39 @@
 import Deck
+import random
 
 class Hand:
-    def __init__(self, deck):
+    def __init__(self, deck, card1 = 0, card2 = 0):
+        self.cards = []
+        self.value = 0
+        if card1 != 0 and card2 != 0:
+            self.cards.append(card1)
+            self.cards.append(card2)
 
+        elif card1 != 0:
+            self.cards.append(card1)
+
+        elif card2 != 0:
+            self.cards.append(card2)
+
+        else:
+            self.cards.append(deck.deal_and_remove_card())
+            self.cards.append(deck.deal_and_remove_card())
+
+    def calculate_value(self):
+        pass
+
+
+    def draw_card(self):
+        pass
+
+    def hit(self):
+        pass
+
+    def double_down(self):
+        pass
+
+    def stand(self):
+        pass
 
 
 class Player():
