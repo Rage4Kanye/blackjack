1b01dc4e827effe12cdb9cd2576420de7ed84593
DLK <dkratzk@gmail.com>
2018-06-06 18:26:18 -0500

    new file

---
+++ Actions.py
@@ -0,0 +1,9 @@
+class Action:
+    def __init__(self, cards, dealer_cards):
+        self.cards = cards
+        self.dealer_cards = dealer_cards
+        self.choices = {''}
+
+    def choice(self):
+        for value in self.choices:
+            pass
